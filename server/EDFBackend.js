83f7afd6b132b9d380633795e1c7101bf14a0721
Elham <elhambadri24@gmail.com>
2022-05-05 15:12:07 -0400

    fixed some bugs

--- server/EDFBackend.js
+++ server/EDFBackend.js
@@ -234,7 +234,7 @@ let WFDB = {
       }
       const numSamples = rows.length;
       const samplingRate = Math.round(numSamples / outputDurationInSeconds);
-      console.log("237 samplingRate : " + samplingRate);
+      console.log("237 samplingRate : " + samplingRate); console.log("237 numSamples : " + numSamples); console.log("237 outputDurationInSeconds : " + outputDurationInSeconds);
       const data = channelNames.map(() => {
         return new FloatArrayType(numSamples);
       });
